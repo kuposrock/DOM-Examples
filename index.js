b0724c7670d3746d8134c4a8ed2236fab4e94d8d
Matt <kuposrock@gmail.com>
2021-07-27 17:36:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var header = document.getElementById("header");
+header.innerHTML = "<center><h1><b>JavaScript Made This!!!</h1><p><span>Matthew Grahek </span>Wrote the Javascript</b></p></center>";
+
+document.getElementsByClassName("left")[0].innerHTML = "Im made of Dragon Skin.";
+document.getElementsByClassName("left")[1].innerHTML = "I am an FBI agent hiding in the forest";
+document.getElementsByClassName("right")[0].innerHTML = "Im not a Dragon am a warrior.";
+document.getElementsByClassName("right")[1].innerHTML = "Im also an ex-FBI crime fighter.";
+
